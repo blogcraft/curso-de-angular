5f2de2f10f9b68f120be32fd5e5f3261962f2b21
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-04-15 19:06:27 -0400

    7.1. Crear usuarios ficticios

---
+++ src/app/usuarios-ficticios.ts
@@ -0,0 +1,14 @@
+import { Usuario } from "./usuario";
+
+export const USUARIOS: Usuario[] = [
+    { id: 1, nombre: "Juan" },
+    { id: 2, nombre: "Pepe" },
+    { id: 3, nombre: "Angelica" },
+    { id: 4, nombre: "Macarena" },
+    { id: 5, nombre: "Pedro" },
+    { id: 6, nombre: "Margarita" },
+    { id: 7, nombre: "Carlos" },
+    { id: 8, nombre: "Daniela" },
+    { id: 9, nombre: "Carolina" },
+    { id: 10, nombre: "Rafael" },
+];
