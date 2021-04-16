0da8231e22ba7161cf9f2162328c974e53c79175
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-04-15 21:25:44 -0400

    9.1.2. Obtener data de usuario

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { Usuario } from './usuario';
+import { USUARIOS } from './usuarios-ficticios';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
   constructor() { }
+
+  obtenerUsuarios(): Usuario[] {
+    return USUARIOS;
+  }
 }
