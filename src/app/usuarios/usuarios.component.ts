ac14ae234ae694225dfad1ca56ba688b8df5dfb0
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-04-16 14:10:04 -0400

    10.5.5. Eliminar código muerto

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { MensajeService } from '../mensaje.service';
 import { UsuarioService } from '../usuario.service';
 
 import { Usuario } from '../usuario';
@@ -12,20 +11,13 @@ import { Usuario } from '../usuario';
 })
 export class UsuariosComponent implements OnInit {
   usuarios: Usuario[] = [];
-  usuarioSeleccionado?: Usuario;
 
   constructor(
     private usuarioService: UsuarioService,
-    private mensajeService: MensajeService
   ) { }
 
   ngOnInit(): void {
     this.obtenerUsuarios();
-  }
-
-  onSelect(usuario: Usuario): void {
-    this.usuarioSeleccionado = usuario;
-    this.mensajeService.agregar(`UsuariosComponent: Usuario seleccionado id=${usuario.id}`)
   }
 
   obtenerUsuarios(): void {
