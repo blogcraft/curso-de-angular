260d16f830f50c8e5d14ba31e2723822808dff16
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-04-16 13:45:13 -0400

    10.5.2. Agregar ruta a detalle usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsuarioDetalleComponent } from './usuario-detalle/usuario-detalle.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 const rutas: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'usuarios', component: UsuariosComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'usuarios/:id', component: UsuarioDetalleComponent }
 ]
 
 @NgModule({
