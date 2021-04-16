550685246f5747199d57e0637b0468d70400cbe0
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-04-16 12:57:31 -0400

    10.4.1. Agregar la ruta al Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 const rutas: Routes = [
-  { path: 'usuarios', component: UsuariosComponent }
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ]
 
 @NgModule({
